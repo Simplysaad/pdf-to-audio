2eb98268fc1d460eb6eae21a0a313c9daa024164
simplysaad <saadidris70@gmail.com>
2025-08-28 14:20:53 +0100

    fix: removed imports

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@
 
 import TelegramBot from "node-telegram-bot-api";
 import fs from "fs";
-import { createAudio, splitChapters, uploadAudio } from "./Utils/index.js";
+// import { createAudio, splitChapters, uploadAudio } from "./Utils/index.js";
 import Upload from "./Models/upload.model.js";
 
 const token = process.env.TELEGRAM_BOT_KEY;
